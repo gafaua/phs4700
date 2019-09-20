0065c9fef4647b56e100c1d59b70a56abfa19092
mechai <jeyzermc@gmail.com>
2019-09-20 17:46:38 -0400

    cm

--- tp1/Devoir1.m
+++ tp1/Devoir1.m
@@ -1,6 +1,6 @@
 function [pcm, MI, aa] = Devoir1(pos, mu, va, fi)
     % TEMP
-    pcm = [5; 5; 5];
-    MI = [7 7 7; 7 7 7; 7 7 7]
+    pcm = CentreMasse.CMTotal(pos, mu);
+    MI = [7 7 7; 7 7 7; 7 7 7];
     aa = [9; 9; 9];
 end
