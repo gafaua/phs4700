3ba6db6a8121960ed53d51aa38d561ad6cdb9349
mechai <jeyzermc@gmail.com>
2019-09-22 09:32:58 -0400

    Yes

--- tp1/Devoir1.m
+++ tp1/Devoir1.m
@@ -1,6 +1,7 @@
 function [pcm, MI, aa] = Devoir1(pos, mu, va, fi)
     % TEMP
     pcm = CentreMasse.CMTotal(pos, mu);
-    MI = [7 7 7; 7 7 7; 7 7 7];
-    aa = [9; 9; 9];
+    MI = MomentInertie.calculerInertieTotal(mu)
+    % aa = AA(MI, fi, va);
+    aa = [9; 9; 9];    
 end
--- tp1/MomentInertie.m
+++ tp1/MomentInertie.m
@@ -5,27 +5,28 @@ classdef MomentInertie
 
     methods (Static = true)
 
-        function momentInertieTotal = calculerInertieTotal(centreMasseTotal)
-            [bras1 bras2 bras3 bras4] = centreMasseObjet(Constantes.rayon_bras, Constantes.longueur_bras / 2)
-            [moteur1 moteur2 moteur3 moteur4] = centreMasseObjet(Constantes.hauteur_moteur / 2, Constantes.longueur_bras + Constantes.rayon_moteur)
+        function momentInertieTotal = calculerInertieTotal(mu)
+            [bras1 bras2 bras3 bras4] = MomentInertie.centreMasseObjet(Constantes.rayon_bras, Constantes.longueur_bras / 2)
+            [moteur1 moteur2 moteur3 moteur4] = MomentInertie.centreMasseObjet(Constantes.hauteur_moteur / 2, Constantes.longueur_bras + Constantes.rayon_moteur)
             centreMasseSphere = CentreMasse.cm_sphere()
             centreMasseColis = CentreMasse.cm_colis()
 
-            inertieDemiSphere = calculerMIDemisphere() + Constantes.masse_sphere * calculerEcartInertie(centreMasseSphere)
-            inertieColis = calculerMIparralelepipede() + Constantes.centreMasseColis * calculerEcartInertie(centreMasseColis)
+            inertieDemiSphere = MomentInertie.calculerMIDemisphere() + Constantes.masse_sphere * MomentInertie.calculerEcartInertie(centreMasseSphere)
+            inertieColis = MomentInertie.calculerMIparralelepipede() + Constantes.masse_colis * MomentInertie.calculerEcartInertie(centreMasseColis)
 
-            inertieBras1 = calculerMIBras(true) + Constantes.masse_bras * calculerEcartInertie(bras1)
-            inertieBras2 = calculerMIBras()     + Constantes.masse_bras * calculerEcartInertie(bras2)
-            inertieBras3 = calculerMIBras(true) + Constantes.masse_bras * calculerEcartInertie(bras3)
-            inertieBras4 = calculerMIBras()     + Constantes.masse_bras * calculerEcartInertie(bras4)
+            inertieBras1 = MomentInertie.calculerMIBras(true)  + Constantes.masse_bras * MomentInertie.calculerEcartInertie(bras1);
+            inertieBras2 = MomentInertie.calculerMIBras(false) + Constantes.masse_bras * MomentInertie.calculerEcartInertie(bras2);
+            inertieBras3 = MomentInertie.calculerMIBras(true)  + Constantes.masse_bras * MomentInertie.calculerEcartInertie(bras3);
+            inertieBras4 = MomentInertie.calculerMIBras(false) + Constantes.masse_bras * MomentInertie.calculerEcartInertie(bras4);
 
-            inertieMoteur1 = calculerMIMoteur() + Constantes.masse_moteur * calculerEcartInertie(moteur1)
-            inertieMoteur2 = calculerMIMoteur() + Constantes.masse_moteur * calculerEcartInertie(moteur2)
-            inertieMoteur3 = calculerMIMoteur() + Constantes.masse_moteur * calculerEcartInertie(moteur3)
-            inertieMoteur4 = calculerMIMoteur() + Constantes.masse_moteur * calculerEcartInertie(moteur4)
+            inertieMoteur1 = MomentInertie.calculerMIMoteur() + Constantes.masse_moteur * MomentInertie.calculerEcartInertie(moteur1);
+            inertieMoteur2 = MomentInertie.calculerMIMoteur() + Constantes.masse_moteur * MomentInertie.calculerEcartInertie(moteur2);
+            inertieMoteur3 = MomentInertie.calculerMIMoteur() + Constantes.masse_moteur * MomentInertie.calculerEcartInertie(moteur3);
+            inertieMoteur4 = MomentInertie.calculerMIMoteur() + Constantes.masse_moteur * MomentInertie.calculerEcartInertie(moteur4);
 
-            momentInertieTotal = inertieDemiSphere + inertieColis + inertieBras1 + inertieBras2 + inertieBras3 + inertieBras4 + inertieMoteur1 + inertieMoteur2 + inertieMoteur3 + inertieMoteur4
-
+            MI = inertieDemiSphere + inertieColis + inertieBras1 + inertieBras2 + inertieBras3 + inertieBras4 + inertieMoteur1 + inertieMoteur2 + inertieMoteur3 + inertieMoteur4;
+            R2 = [cos(mu), 0, sin(mu); 0, 1, 0; -sin(mu), 0, cos(mu)];
+            momentInertieTotal = R2 * MI * R2';
         endfunction
 
         function demiSphereMI = calculerMIDemisphere()
@@ -52,7 +53,6 @@ classdef MomentInertie
             end
 
             brasMI = [MIx  0 0; 0 MIy 0; 0 0 MIz]
-
         endfunction
 
         function moteurMI = calculerMIMoteur()
@@ -76,7 +76,6 @@ classdef MomentInertie
             MIz = 1/12 * Constantes.masse_colis * (largeurSquared + longueurSquared)
 
             parralelepipedeMI = [MIx 0 0; 0 MIy 0; 0 0 MIz]
-
         endfunction
 
         function [obj1 obj2 obj3 obj4] = centreMasseObjet(rayon, longueur)
@@ -88,13 +87,9 @@ classdef MomentInertie
         endfunction
 
         function ecartInertie = calculerEcartInertie(vecteur)
-
             ecartInertie = [vecteur(2)^2 + vecteur(3)^2, -vecteur(1)*vecteur(2), -vecteur(1)*vecteur(3); 
                             -vecteur(2)*vecteur(1), vecteur(1)^2 + vecteur(3)^2, -vecteur(2)*vecteur(3);
                             -vecteur(3)*vecteur(1), -vecteur(3)*vecteur(2), vecteur(1)^2 + vecteur(2)^2];
-
         endfunction
-
     endmethods
-
 end
