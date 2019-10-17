408c4bb6e9f049d2140bc81c7f5c35dff7e42702
Mehdi <jeyzermc@gmail.com>
2019-10-17 18:52:46 -0400

    MC: Plot results

--- tp2/Devoir2.m
+++ tp2/Devoir2.m
@@ -3,7 +3,7 @@ function [coup, vbf, t, rbt] = Devoir2(option, xy0, vb0, wb0)
 	vbf = [vb0]';
 	t = [0];
 
-	dt = 0.0001;
+	dt = 0.001;
 	i = 2;
 
 	pos = 4;
@@ -24,6 +24,43 @@ function [coup, vbf, t, rbt] = Devoir2(option, xy0, vb0, wb0)
 		i += 1;
 	end
 
+	clf;
+	plot3(rbt(:, 1), rbt(:, 2), rbt(:, 3), "o");
+	hold on;
+
+	% First rectangle
+	h = patch([0, 0, 60, 60], [0, 70, 70, 0], [0, 0, 0, 0], 'green');
+	set(h,'EdgeColor','none');
+	hold on;
+
+	% Second rectangle
+	h = patch([60, 60, 110, 110], [45, 70, 70, 45], [0, 0, 0, 0], 'green');
+	set(h,'EdgeColor','none');
+	hold on;
+
+	% Triangle
+	h = patch([60, 60, 110], [0, 45, 45], [0, 0, 0], 'green');
+	set(h,'EdgeColor','none');
+	hold on;
+
+	% Vert
+	t = linspace(0,2*pi,1000)'; 
+	circsx = 15.*cos(t) + 92; 
+	circsy = 15.*sin(t) + 53; 
+	h = plot(circsx,circsy, 'k'); 
+	hold on;
+
+	% Coupe
+	t = linspace(0,2*pi,1000)'; 
+	circsx = 0.54.*cos(t) + 92; 
+	circsy = 0.54.*sin(t) + 53; 
+	circsz = ones(1, 1000) * 0.1;
+	h = patch(circsx,circsy, circsz, 'k'); 
+	hold on;
+
+	title (strcat("Figure ", mat2str(option)));
+
+	% pause(1);
 	coup = pos;
 	vbf = vbf(i-1, :);
 end
