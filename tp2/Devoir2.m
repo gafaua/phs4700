b01c14c6e98030be27e8e7d4e1f07b13dd192f7f
Gaspar Faure <gasparfaure@hotmail.com>
2019-10-17 16:30:04 -0400

    ÇA MARCHE PEUT-ETRE yeeet

--- tp2/Devoir2.m
+++ tp2/Devoir2.m
@@ -1,37 +1,50 @@
 function [coup, vbf, t, rbt] = Devoir2(option, xy0, vb0, wb0)
-	rbt = [xy0; 0]';
+	rbt = [xy0', 0.02135];
 	vbf = [vb0]';
 	t = [0];
 
-	deltaT = 0.001;
-	i = 1;
+	dt = 0.0001;
+	i = 2;
 
-	while true
-		t = [t; t(i) + deltaT];
+	pos = 4;
 
-		% CALCULER ACCELERATION SELON LES FORCES
-		accel = CalculerAcceleration(option, vbf(i,:), wb0');
+	while (pos == 4)
+		t = [t; t(i-1) + dt];
 
-		% CALCULER VITESSE SELON ACCELERATION
-		vf = vbf(i,:) + accel * deltaT;
-		vbf = [vbf; vf];
+		q = [vbf(i-1, :); rbt(i-1, :); wb0'];
 
-		% CALCULER POSITION SELON VITESSE
-		rbtf = rbt(i, :) + vbf(i, :) * deltaT + 0.5 * accel * deltaT ^ 2;
-		rbt = [rbt; rbtf];
+		qf = RK4(option, q, dt);
 
-		% VERIFIER POSITION BALLE
-		pos = PositionBalle(rbtf);
+		rbt = [rbt; qf(2, :)];
+		vbf = [vbf; qf(1, :)];
 
-		if pos == 3 || rbt(i+1, 3) <= 0
-			coup = pos;
-			break;
-		end
+		% Vérifier position balle
+		pos = PositionBalle(rbt(i, :));
 
-		% TODO: CHECKER SI POSITION EST SUR LA COUPE HAUTEUR
-
-		i += 1
+		i += 1;
 	end
+
+	coup = pos;
+	vbf = vbf(i-1, :);
+end
+
+function qf = RK4(option, q, dt)
+	%k1 = g(q(tn-1), tn-1)
+	k1 = g(option, q, 0);
+	%k2 = g(q(tn-1) + Deltat/2 * k1, tn-1 + Deltat/2)
+	k2 = g(option, q + k1 * dt/2, dt/2);
+	%k3 = g(q(tn-1) + Deltat/2 * k2, tn-1 + Deltat/2)
+	k3 = g(option, q + k2 * dt/2, dt/2);
+	%k4 = g(q(tn-1) + Deltat * k3, tn-1 + Deltat)
+	k4 = g(option, q + k3 * dt, dt);
+
+	qf = q + dt/6 * (k1 + 2*k2 + 2*k3 + k4);
+end
+
+function g = g(option, q, dt)
+	a = CalculerAcceleration(option, q(1, :), q(3,:));
+	v = q(1,:) + a * dt;
+	g = [a; v; [0, 0, 0]];
 end
 
 function accel = CalculerAcceleration(option, vb, wb) 
--- tp2/PositionBalle.m
+++ tp2/PositionBalle.m
@@ -1,30 +1,33 @@
 function pos = PositionBalle(rbt)
-    if DansCoupe(rbt(1), rbt(2))
-        pos = 0; % COUPE
-    elseif DansVert(rbt(1), rbt(2))
-        pos = 1; % VERT
+    if DansVert(rbt(1), rbt(2), rbt(3))
+        if DansCoupe(rbt(1), rbt(2))
+            pos = 0; % COUPE
+        else
+            pos = 1; % VERT
+        end
     elseif Exterieur(rbt(1), rbt(2))
         pos = 3; % EXTERIEUR
-    else
+    elseif Terrain(rbt(3))
         pos = 2; % TERRAIN
+    else
+        pos = 4; % ENCORE DANS LES AIRS
     end
     % EXPLIQUER QU'ON CHECK JUSTE LE CENTRE DE MASSE
 end
 
 function dansCoupe = DansCoupe(x, y)
-    posX = (x - (110 - 18)) ^ 2;
-    posY = (y - (70 - 17)) ^ 2;
-    rayonC = 0.054 ^ 2;
-    dansCoupe = (posX + posY <= rayonC);
-    % DOIT ON CALCULER Z ?
+    posX = (x - (110 - 18));
+    posY = (y - (70 - 17));
+    rayonC = 0.054;
+    dansCoupe = (posX^2 + posY^2 <= rayonC^2);
 end
 
-function dansVert = DansVert(x, y)
-    posX = (x - (110 - 18)) ^ 2;
-    posY = (y - (70 - 17)) ^ 2;
-    rayonC = 15 ^ 2;
-    dansVert = (posX + posY <= rayonC);
-    % DOIT ON CALCULER Z ?
+function dansVert = DansVert(x, y, z)
+    R = 237.25 / 7; % RAYON DE LA SPHERE TOTALE
+    posX = (x - (110 - 18));
+    posY = (y - (70 - 17));
+    posZ = (z - (3.5 - R));
+    dansVert = (posX^2 + posY^2 + posZ^2 <= (R + 0.02135)^2);
 end
 
 function exterieur = Exterieur(x, y)
@@ -33,5 +36,8 @@ function exterieur = Exterieur(x, y)
                 x > 110 || ...
                 y > 70 || ...
                 (x > 60 && y < 45/50 * (x - 60));
-    % DOIT ON CALCULER Z?
+end
+
+function terrain = Terrain(z)
+    terrain = (z <= 0.02135);
 end
