0ef998e6428619cf98c1baf9022ac5a1ce9cb9e3
Gaspar Faure <gasparfaure@hotmail.com>
2019-11-09 17:12:43 -0500

    Détection des collisions finie, à tester

--- tp3/Position.m
+++ tp3/Position.m
@@ -1,5 +1,6 @@
-function pos = Position(pos_bloc, pos_balle, t, w_bloc)
-    if Collision(pos_bloc, pos_balle, t, w_bloc)
+function [pos, point] = Position(pos_bloc, pos_balle, t, w_bloc)
+    [collision, point] = Collision(pos_bloc, pos_balle, t, w_bloc);
+    if (collision)
         pos = 0;
     elseif BlocToucheSol(pos_bloc)
         pos = 1;
@@ -9,7 +10,7 @@ function pos = Position(pos_bloc, pos_balle, t, w_bloc)
     end
 end
 
-function collision = Collision(pos_bloc, pos_balle, t, w_bloc)
+function [collision, point] = Collision(pos_bloc, pos_balle, t, w_bloc)
     RC_bloc = 0.37837;  #Distance entre CM du cube et son coin
     RC_balle = 0.02;
 
@@ -17,86 +18,114 @@ function collision = Collision(pos_bloc, pos_balle, t, w_bloc)
     Rtot = RC_balle + RC_bloc;
     
     if (d <= Rtot)
-        % TODO: Continuer avec detection de collision plus approfondi
+        % Detection de collision plus approfondie
         M = Rotation(t, w_bloc);
-
-        %Sommets du cube nécessaires pour former les différents plans du cube
-        A = M * [-0.03, -0.03,  0.03] + pos_bloc;
-        B = M * [0.03,  -0.03,  0.03] + pos_bloc;
-        C = M * [0.03,  0.03,   0.03] + pos_bloc;
-        D = M * [0.03,  0.03,  -0.03] + pos_bloc;
-        E = M * [-0.03, -0.03, -0.03] + pos_bloc;
-        F = M * [-0.03, 0.03,  -0.03] + pos_bloc;
-
-        Plan1 = CollisionPlan(A, B, C);
-        Plan2 = CollisionPlan(A, B, E);
-        Plan3 = CollisionPlan(B, C, D);
-        Plan4 = CollisionPlan(D, E, F);
-        Plan5 = CollisionPlan(C, D, F);
-        Plan6 = CollisionPlan(A, E, F);
+        c = 0.03;
         
+        %Sommets S du cube
+        S = [M * [-c, -c,  c] + pos_bloc];
+        S = [S; M * [c,  -c,  c] + pos_bloc];
+        S = [S; M * [c,  c,   c] + pos_bloc];
+        S = [S; M * [c,  c,  -c] + pos_bloc];
+        S = [S; M * [-c, -c, -c] + pos_bloc];
+        S = [S; M * [-c, c,  -c] + pos_bloc];
+        S = [S; M * [c, -c,  -c] + pos_bloc];
+        S = [S; M * [-c, c,  c] + pos_bloc];
+
+        %Détection des collisions possibles avec les sommets du cube
+        for idx=1:8
+            if (CollisionPointSphere(S(idx, :), pos_balle))
+                Collision = 1;
+                point = S(idx, :);
+                return;
+            end
+        end
+
+        %Détection des collisions possibles avec les arêtes du cube
+        [collision, point] = CollisionAreteSphere(S(1, :), S(2, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(1, :), S(8, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(1, :), S(5, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(3, :), S(2, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(7, :), S(2, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(7, :), S(5, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(7, :), S(4, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(4, :), S(3, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(6, :), S(4, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(6, :), S(5, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(6, :), S(8, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionAreteSphere(S(8, :), S(3, :), pos_balle);
+        if (collision) return; end
+
+        %Détection des collusions possibles avec les faces du cube
+        [collision, point] = CollisionPlanSphere(S(3, :), S(2, :), S(1, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionPlanSphere(S(5, :), S(1, :), S(2, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionPlanSphere(S(2, :), S(3, :), S(4, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionPlanSphere(S(4, :), S(6, :), S(5, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionPlanSphere(S(6, :), S(4, :), S(3, :), pos_balle);
+        if (collision) return; end
+        [collision, point] = CollisionPlanSphere(S(1, :), S(5, :), S(6, :), pos_balle);
+        if (collision) return; end
     end 
 
     collision = 0;
+    point = [0, 0, 0];
 end
 
-function Plan = CollisionPlan(P1, P2, P3, pos_balle)
-    RC_balle = 0.02;
-    
-    % Calcul de normal
-    normal = cross(P1-P2, P1-P3);
-
-    % Vecteur unitair normal
-    vu_normal = normal / sqrt(normal(1,:)^2 + normal(2,:)^2 + normal(3,:)^2);
-    
-    
-    
-    % P = [x, y, z]
-    % plan = normal * P'
-    
-
-end
-
-function M = Rotation(t, w_bloc)
-    M = [0,          -w_bloc[3], w_bloc[2];...
-         w_bloc[3],  0,          -w_bloc[1];...
-         -w_bloc[2], w_bloc[1],  0] * t;
-end
-
-function Collision = CollisionCoin(P, pos_balle, r_balle = 0.02)
+function Collision = CollisionPointSphere(P, pos_balle, r_balle = 0.02)
     %sphère: (x-pos[x])^2 + (y-pos[y])^2 + (z-pos[z])^2 <= r
-    Collision = ((P[1] - pos_balle[1])^2 + (P[2] - pos_balle[2])^2 + (P[3] - pos_balle[3])^2) <= r_balle;
+    Collision = ((P(1) - pos_balle(1))^2 + (P(2) - pos_balle(2))^2 + (P(3) - pos_balle(3))^2) <= r_balle;
 end
 
-function [Collision, Point] = CollisionArete(P1, P2, pos_balle, r_balle = 0.02)
+%TODO FINIR COLLISION AVEC ARETE SI 2 POINTS DE CONTACT
+function [Collision, Point] = CollisionAreteSphere(P1, P2, pos_balle, r_balle = 0.02)
     u = P1 - P2;
     
     %En se basant sur l'équation de la sphère
     % et sur l'équation de la droite passant par P1 de vecteur directeur u
-    a = u[1]^2 + u[2]^2 + u[3]^2;
-    b = (2 * u[1] * (pos_balle[1] - P1[1])) + (2 * u[2] * (pos_balle[2] - P1[2])) + (2 * u[3] * (pos_balle[3] - P1[3]));
-    c = (P1[1] - pos_balle[1])^2 + (P1[2] - pos_balle[2])^2 + (P1[3] - pos_balle[3])^2 - r_balle;
+    a = u(1)^2 + u(2)^2 + u(3)^2;
+    b = (2 * u(1) * (pos_balle(1) - P1(1))) + (2 * u(2) * (pos_balle(2) - P1(2))) + (2 * u(3) * (pos_balle(3) - P1(3)));
+    c = (P1(1) - pos_balle(1))^2 + (P1(2) - pos_balle(2))^2 + (P1(3) - pos_balle(3))^2 - r_balle;
 
     facteurs = roots([a b c])
     if (isreal(coll))
-        if (facteurs[1] == facteurs[2])
-            Point = P1 + u * facteurs[1];
-            Collision = PointEntrePoints(Point);
+        if (facteurs(1) == facteurs(2))
+            Point = P1 + u * facteurs(1);
+            Collision = PointSurArete(Point);
         else    %Probleme TODO: figure out quoi faire si deux points sont en intersection avec une arete
-            p_1 = P1 + u * facteurs[1];
-            p_2 = P1 + u * facteurs[2];
+            p_1 = P1 + u * facteurs(1);
+            p_2 = P1 + u * facteurs(2);
 
-            if (PointEntrePoints(p_1))
-                if (PointEntrePoints(p_2)) %intersection de 2 points sur l'arete, point milieu?
-                    Point = (p_1 + p_2) / 2;
-                    Collision = 1; 
-                else
-                    Point = p_1;
-                    Collision = 1;
-                end
-            elseif(PointEntrePoints(p_2))
-                Point = p_2;
-                Collision = 1;
+            Point = [0, 0, 0];
+            n = 0;
+
+            if (PointSurArete(p_1))
+                Point += p_1;
+                n += 1;
+            end
+
+            if (PointSurArete(p_2))
+                Point += p_2;
+                n += 1;
+            end
+
+            if (n)
+                Collision = n;%n = 2 implique que 2 points sont en collision avec l'arête
+                Point = Point / n;
             else
                 Collision = 0;
             end
@@ -106,12 +135,43 @@ function [Collision, Point] = CollisionArete(P1, P2, pos_balle, r_balle = 0.02)
     end
 end
 
-function Entre = PointEntrePoints(P, P1, P2)
+%P2 est toujours le coin de l'angle droit du triangle formé par P1 P2 P3
+function [Collision, Point] = CollisionPlanSphere(P1, P2, P3, pos_balle, r_balle = 0.02)
+    % Calcul de la normale n du plan et de la constante D de l'équation du plan
+    n = cross(P1-P2, P1-P3);
+    D = -(n(1)*P1(1) + n(2)*P1(2) + n(3)*P1(3));
+
+    %En utilisant la droite passant par pos_balle de vecteur directeur n, trouver le point d'intersection
+    t = (-D - (pos_balle(1)*n(1)) - (pos_balle(2)*n(2)) - (pos_balle(3)*n(3))) / (n(1)^2 + n(2)^2 + n(3)^2);
+    %Point d'intersection P
+    P = pos_balle + t * n;
+
+    %Vérifier si le point est à l'intérieur des limites du bloc et dans ou sur la surface de la balle
+    Collision = PointSurFace(P, P1, P2, P3) && CollisionPointSphere(P, pos_balle);
+
+    if (Collision)
+        Point = P;
+    end
+end
+
+function Entre = PointSurArete(P, P1, P2)
     norme_u = norm(P1-P2);
-    norme_v_1 = norm(P1-P);
-    norme_v_2 = norm(P2-P);
-    
-    Entre = norme_v_1 < norme_u && norme_v_2 < norme_u;
+
+    Entre = norm(P1-P) < norme_u && norm(P2-P) < norme_u;
+end
+
+%Cette fonction prends en compte que P2 est le point de l'angle droit du triangle formé par P1 P2 P3
+function Entre = PointSurFace(P, P1, P2, P3)
+    P4 = P3 + (P1 - P2);
+    norme_u = norm(P1 - P3);
+
+    Entre = norm(P1-P) <= norme_u && norm(P2-P) <= norme_u && norm(P3-P) <= norme_u && norm(P4-P) <= norme_u;
+end
+
+function M = Rotation(t, w_bloc)
+    M = [0,          -w_bloc(3), w_bloc(2);...
+         w_bloc(3),  0,          -w_bloc(1);...
+         -w_bloc(2), w_bloc(1),  0] * t;
 end
 
 function blocToucheSol = BlocToucheSol(pos_bloc)
