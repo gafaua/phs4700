b324b8c12d425143eb744513fcb8f9e2e216a261
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2019-11-10 12:18:57 -0500

    ajout de inertie de la balle

---
+++ tp3/ApresCollision.m
@@ -0,0 +1,8 @@
+function [vf_bloc, wf_bloc, vf_balle] = ApresCollision()
+
+    I_balle = [0.000112 0        0; ...
+               0        0.000112 0; ...
+               0        0        0.000112];
+
+
+end
